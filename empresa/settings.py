bf17e78b0d91345434ebb41ff30ea83ef3a894a5
Irvin <al169890@alumnos.uacj.mx>
2023-11-27 14:41:20 -0700

    cambio

--- empresa/settings.py
+++ empresa/settings.py
@@ -147,6 +147,15 @@ if 'RENDER_EXTERNAL_HOSTNAME' in os.environ:
     STATIC_URL = f'https://{RENDER_EXTERNAL_HOSTNAME}{STATIC_URL}'
     MEDIA_URL = f'https://{RENDER_EXTERNAL_HOSTNAME}{MEDIA_URL}'
 
+if not DEBUG:
+    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
