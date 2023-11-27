ea44974d296932e63437adb643b50f5ccdead724
Irvin <al169890@alumnos.uacj.mx>
2023-11-27 15:33:04 -0700

    SIN IF

--- empresa/urls.py
+++ empresa/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     path('balance/',include('balance.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
