a141bd710d265821778aae3d1966754cc8dcb51b
Irvin Dominguez <al169890@alumnos.uacj.mx>
2023-11-27 04:00:05 -0600

    sale back

--- back_empresa/balance/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import BalanceListCreateView, HistorialBalanceListCreateView
-
-urlpatterns = [
-    path('balance/', BalanceListCreateView.as_view(), name='balance-list-create'),
-    path('balances/<int:pk>/historial/', HistorialBalanceListCreateView.as_view(), name='historialbalance-list-create'),
-    
-    
-]
