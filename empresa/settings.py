171f1b7c6a2ee27308524c1d97ff55486475c1b9
Irvin <al169890@alumnos.uacj.mx>
2023-11-27 12:26:21 -0700

    nueva url

--- empresa/settings.py
+++ empresa/settings.py
@@ -179,8 +179,8 @@ SIMPLE_JWT={
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",  # Agrega aquí el origen de tu aplicación Angular
-    "https://heartfelt-pie-c21db3.netlify.app",
+    "https://ecbooks.netlify.app",
 
 ]
 
-CSRF_TRUSTED_ORIGINS = ["https://heartfelt-pie-c21db3.netlify.app"]
+CSRF_TRUSTED_ORIGINS = ["https://ecbooks.netlify.app"]
