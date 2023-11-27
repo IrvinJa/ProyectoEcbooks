569446e45cde8766c59cd81c1cb3ea9156476411
Irvin <al169890@alumnos.uacj.mx>
2023-11-27 15:43:16 -0700

    nueva mediaurl

--- empresa/urls.py
+++ empresa/urls.py
@@ -19,3 +19,4 @@ urlpatterns = [
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
